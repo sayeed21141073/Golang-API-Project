443e182e81138e09696d0dc5e227b0468025dd72
Sayeed Shuvo <md.sayeed.ibne.zaman@g.bracu.ac.bd>
2023-01-02 23:04:40 +0600

    project files

---
+++ database/db.go
@@ -0,0 +1,138 @@
+// package main
+
+// import (
+// 	"database/sql"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+
+// 	_ "github.com/go-sql-driver/mysql"
+// )
+
+// func main() {
+// 	// Connect to the database
+// 	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/prayertime")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
+
+// 	// Select the rows you want to convert and decode as JSON
+// 	rows, err := db.Query("SELECT id, fajr, dhuhr, asr, maghrib, isha FROM timings")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer rows.Close()
+
+// 	// Iterate over the selected rows
+// 	for rows.Next() {
+// 		// Declare variables to hold the selected values
+// 		var id int
+// 		var fajr, dhuhr, asr, maghrib, isha string
+// 		// Scan the selected values into the variables
+// 		err = rows.Scan(&id, &fajr, &dhuhr, &asr, &maghrib, &isha)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		// Convert the values to the desired type (e.g. int64)
+// 		// and store them in a map
+// 		m := map[string]interface{}{
+// 			"id":      id,
+// 			"fajr":    fajr,
+// 			"dhuhr":   dhuhr,
+// 			"asr":     asr,
+// 			"maghrib": maghrib,
+// 			"isha":    isha,
+// 		}
+// 		// Encode the map as JSON
+// 		jsonData, err := json.Marshal(m)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		// Do something with the JSON data (e.g. print it to the console)
+// 		fmt.Println(string(jsonData))
+// 	}
+
+// }
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Connect to the database
+func connectToDatabase() *sql.DB {
+	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/prayertime")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+// Handler function to handle HTTP requests and return the
+// output of the MySQL query as the response
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Connect to the database
+	db := connectToDatabase()
+	defer db.Close()
+
+	// Select the rows you want to convert and decode as JSON
+	rows, err := db.Query("SELECT id, fajr, dhuhr, asr, maghrib, isha FROM timings")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Create a slice to hold the rows
+	var values []map[string]interface{}
+
+	// Iterate over the selected rows
+	for rows.Next() {
+		// Declare variables to hold the selected values
+		var id int
+		var fajr, dhuhr, asr, maghrib, isha string
+
+		// Scan the selected values into the variables
+		err = rows.Scan(&id, &fajr, &dhuhr, &asr, &maghrib, &isha)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Convert the values to the desired type (e.g. int64)
+		// and add them to the slice
+		row := map[string]interface{}{
+			"id":      id,
+			"fajr":    fajr,
+			"dhuhr":   dhuhr,
+			"asr":     asr,
+			"maghrib": maghrib,
+			"isha":    isha,
+		}
+		values = append(values, row)
+	}
+
+	// Encode the slice as JSON
+	jsonData, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set the content type and write the JSON data to the response
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func main() {
+	// Create a new HTTP server and set the handler function
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(handler),
+	}
+	// Start the server
+	server.ListenAndServe()
+}
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Timings struct {
+	Fajr    string `json:"Fajr"`
+	Dhuhr   string `json:"Dhuhr"`
+	Asr     string `json:"Asr"`
+	Maghrib string `json:"Maghrib"`
+	Isha    string `json:"Isha"`
+}
+
+type Data struct {
+	Timings Timings `json:"timings"`
+}
+
+type Response struct {
+	Data Data `json:"data"`
+}
+
+func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/prayertime")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	url := "https://api.aladhan.com/v1/timingsByCity?city=Dhaka&country=Bangladesh"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	body, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	var r Response
+	json.Unmarshal(body, &r)
+
+	// fajr := r.Data.Timings.Fajr
+	// dhuhr := r.Data.Timings.Dhuhr
+	// asr := r.Data.Timings.Asr
+	// maghrib := r.Data.Timings.Maghrib
+	// isha := r.Data.Timings.Isha
+
+	// stmt, err := db.Prepare("INSERT INTO timings(fajr, dhuhr, asr, maghrib, isha) VALUES(?, ?, ?, ?, ?)")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer stmt.Close()
+
+	// _, err = stmt.Exec(fajr, dhuhr, asr, maghrib, isha)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	fmt.Println("Record inserted successfully")
+
+	fmt.Println("Fajr:", r.Data.Timings.Fajr)
+	fmt.Println("Dhuhr:", r.Data.Timings.Dhuhr)
+	fmt.Println("Asr:", r.Data.Timings.Asr)
+	fmt.Println("Maghrib:", r.Data.Timings.Maghrib)
+	fmt.Println("Isha:", r.Data.Timings.Isha)
+}
